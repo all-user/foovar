f72e2c03107a6b50d6c0a490aee45f85c6709559
Keita Okamoto <ok@aaa.email.ne.jp>
2016-10-02 00:32:44 +0900

    Remove try and catch block from requiring foovar.js.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 module.exports = function() {
   return function(stylus) {
-    try {
-      stylus.define('foovar', require('./foovar.js'));
-    } catch (e) {
-      console.error(e);
-      throw new Error(e);
-    }
+    stylus.define('foovar', require('./foovar.js'));
   };
 };
