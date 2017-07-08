d9d8825e74e1838ff04857efa9038661f27cf6d7
Keita Okamoto <ok@aaa.email.ne.jp>
2017-07-09 08:18:03 +0900

    Add test for boolean and null

---
+++ test/export_null_var/index.test.js
@@ -0,0 +1,44 @@
+import assert from 'assert';
+import stylus from 'stylus';
+import Promise from 'bluebird';
+import path from 'path';
+import _fs from 'fs';
+import _rimraf from 'rimraf';
+
+const rimraf = Promise.promisify(_rimraf);
+const fs = Promise.promisifyAll(_fs);
+
+const OUT_PATH = path.resolve(__dirname, './vars.js');
+const SOURCE_PATH = path.resolve(__dirname, './vars.styl');
+
+describe('export null var:', () => {
+  let vars;
+  before(() => {
+    return Promise
+      .try(() => rimraf(OUT_PATH))
+      .then(() => fs.readFileAsync(SOURCE_PATH, 'utf8'))
+      .then(stylusStr => {
+        stylus(stylusStr)
+          .use(require(path.resolve(process.cwd(), './src/index.js'))())
+          .render(() => {
+            vars = require(OUT_PATH);
+          });
+      });
+  });
+
+  it('value', () => {
+    assert.strictEqual(vars.varName(), null);
+  });
+
+  it('typeof', () => {
+    assert.strictEqual(typeof vars.varName(), 'object');
+  });
+
+  it('stylus node type', () => {
+    assert.strictEqual(vars.varName.type, 'null');
+  });
+
+  it('stylus css string', () => {
+    assert.strictEqual(vars.varName.css, void 0);
+  });
+});
